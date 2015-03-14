4502b438a55fef01e112512c847224bc4becc42f
Paul Anderson <paul.robert.anderson@gmail.com>
2015-03-15 04:19:02 +1100

    Add Files

---
+++ GenerateHTML.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+path =  Dir.getwd #"C:/" 
+fileExt = "jpg"
+outFile = "files.html"
+columns = 2
+width = 192
+
+puts path
+
+files = Dir.entries(path).select { |f| !f[-fileExt.length..-1].nil? && f[-fileExt.length..-1].downcase  == fileExt.downcase  }
+puts files
+html = "<html><body>"
+html += "\n"
+
+currentColumn=0
+files.each do |file|
+  html += "<a href='#{file}'>"
+  html += "<img src='#{file}' width = #{width}  />"
+  html += "</a>\n"
+  currentColumn += 1
+  if currentColumn==columns  
+    html += "<br/>"
+	currentColumn=0
+  end
+end
+html += "</html></body>"
+puts html
+
+File.open(outFile, 'w') { |file| file.write(html)}
+
+
+STDOUT.flush  
+gets.chomp 
+puts "\a "
